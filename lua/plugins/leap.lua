22cb71154d0a7df830b3fe50f01975f52a4b44b5
vladimirivanov <vl.ivanov1232@gmail.com>
2024-05-31 20:47:35 +0400

    refactor leap

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,20 +1,18 @@
 return {
-	{
-		"ggandor/leap.nvim",
-		enabled = true,
-		keys = {
-			{ "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
-			{ "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
-			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
-		},
-		config = function(_, opts)
-			local leap = require("leap")
-			for k, v in pairs(opts) do
-				leap.opts[k] = v
-			end
-			leap.add_default_mappings(true)
-			vim.keymap.del({ "x", "o" }, "x")
-			vim.keymap.del({ "x", "o" }, "X")
-		end,
+	"ggandor/leap.nvim",
+	enabled = true,
+	keys = {
+		{ "S", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+		{ "s", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+		{ "gs", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
 	},
+	config = function(_, opts)
+		local leap = require("leap")
+		for k, v in pairs(opts) do
+			leap.opts[k] = v
+		end
+		leap.add_default_mappings(true)
+		vim.keymap.del({ "x", "o" }, "x")
+		vim.keymap.del({ "x", "o" }, "X")
+	end,
 }

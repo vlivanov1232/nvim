3cadb1c0529164476a8cde7037202ded2e07f0c2
vladimirivanov <vl.ivanov1232@gmail.com>
2024-05-31 20:34:50 +0400

    added leap, flit, autocommands theme

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -6,3 +6,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function()
+		if vim.g.colors_name == "rose-pine" then
+			require("leap").init_highlight(true)
+			vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+			vim.api.nvim_set_hl(0, "LeapLabelPrimary", { bg = "none", fg = "#03fc49", bold = true })
+			vim.api.nvim_set_hl(0, "LeapLabelSecondary", { bg = "none", fg = "#03fc49", bold = true })
+		end
+	end,
+})
